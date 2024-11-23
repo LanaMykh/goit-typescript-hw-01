860fb3e2925ce8e11353bc87c058a35ba308221b
Svitlana Mykhailiuk <svetiklan@gmail.com>
2024-11-23 10:17:52 +0200

    fixed according to feedback

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -5,6 +5,6 @@
 //   return Object.assign(objA, objB);
 // }
 
-function merge<T, U>(objA: T, objB: U): T & U {
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
     return Object.assign({},objA, objB);
 }
