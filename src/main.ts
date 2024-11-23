3622ce00785eea8f960923aad71f15002d521389
Svitlana Mykhailiuk <svetiklan@gmail.com>
2024-11-23 04:12:46 +0200

    base types

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+
+import './basic/1.ts'
+import './basic/2.ts'
+import './basic/3.ts'
+import './basic/4.ts'
+import './basic/5.ts'
+import './basic/6.ts'
+import './basic/7.ts'
